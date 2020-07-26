860cbe2a20fd2dd0aa3837e9c439d452f749da5d
5MAPTbIH5 <sergeymartun@gmail.com>
2020-07-26 12:29:38 +0300

    Complete home work

--- src/app/mainComponent/app.component.ts
+++ src/app/mainComponent/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Resume } from '../models/Resume';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web-app';
+  resume: Resume = new Resume;
 }
